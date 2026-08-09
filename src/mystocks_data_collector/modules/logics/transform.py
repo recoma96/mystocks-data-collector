d048593dcdba08231bca9c2f5845cda587cfb2b5
recoma <seokbong60@gmail.com>
2026-08-10 08:20:26 +0900

    feat:  보유 종목 현재 상황 레코드 로직 구현

--- src/mystocks_data_collector/modules/logics/transform.py
+++ src/mystocks_data_collector/modules/logics/transform.py
@@ -2,8 +2,14 @@ from typing import Dict
 from uuid import UUID, uuid4
 
 from mystocks_data_collector.config import Config
-from mystocks_data_collector.modules.client.tossinvest_api.responses import TossInvestBuyingPowerResponse, TossInvestCurrentStockPriceResponse, TossInvestOrdersResponse, TossInvestStocksResponse
-from mystocks_data_collector.modules.types import BenchmarkPosition
+from mystocks_data_collector.modules.client.tossinvest_api.responses import (
+    TossInvestBuyingPowerResponse,
+    TossInvestCurrentStockPriceResponse,
+    TossInvestOrdersResponse,
+    TossInvestStocksResponse
+)
+from mystocks_data_collector.modules.types import BenchmarkPosition, Position
+from mystocks_data_collector.modules.utils import now_korea
 
 
 def create_portpolio_by_api_repsonse(
@@ -13,6 +19,7 @@ def create_portpolio_by_api_repsonse(
         res_orders: TossInvestOrdersResponse,
 ):
     new_portpolio_id: UUID = uuid4()
+    today = now_korea()
 
     benchmark_positions = [
         BenchmarkPosition(
@@ -25,4 +32,25 @@ def create_portpolio_by_api_repsonse(
         )
         for ticker, item
         in res_benchmark_prices.items()
+    ]
+
+    positions = [
+        Position(
+            id=uuid4(),
+            portpolio_id=new_portpolio_id,
+            name=item.name,
+            ticker=item.symbol,
+            avg_purchase_price=item.average_purchase_price,
+            current_price=item.last_price,
+            quanity=item.quantity,
+            cost_basis=item.market_value.purchase_amount,
+            market_value=item.market_value.amount,
+            market_value_excluding_fees=item.market_value.amount_after_cost,
+            profit_amount=item.profit_loss.amount,
+            profit_amount_excluding_fees=item.profit_loss.amount_after_cost,
+            profit_rate=item.profit_loss.rate,
+            profit_rate_excluding_fees=item.profit_loss.rate_after_cost,
+            log_date=today,
+        )
+        for item in res_stocks.items
     ]
--- src/mystocks_data_collector/modules/types.py
+++ src/mystocks_data_collector/modules/types.py
@@ -16,9 +16,43 @@ ApiRepsonses:  TypeAlias = Tuple[
 
 @dataclass
 class BenchmarkPosition:
+    """수익 비교용 비교종목
+    """
     id: UUID
     portpolio_id: UUID      # 포트폴리오 UID
     name: str               # 종목명
     ticker: str             # 티커명
     current_price: float     # 현재가
     log_date: datetime      # 로깅 시점
+
+
+@dataclass
+class Position:
+    """보유 종목
+    """
+    id: UUID
+    portpolio_id: UUID                  # 포트폴리오 UUID
+    name: str                           # 종목명
+    ticker: str                         # 티커명
+    avg_purchase_price: float           # 평단가
+    current_price: float                # 현재가
+    quanity: float                      # 보유 주식 수
+    cost_basis: float                   # 매수 금액
+    market_value: float                 # 시장 평가 금액
+    market_value_excluding_fees: float  # 수수료를 제외한 평가금액
+    profit_amount: float                # 손익금액
+    profit_amount_excluding_fees: float # 수수료를 제외한 손익금액
+    profit_rate: float                  # 손익비율
+    profit_rate_excluding_fees: float   # 수수료를 제외한 손익비율
+    log_date: datetime
+
+    def to_str(self):
+        profit_amount_prefix = "-" if self.profit_amount_excluding_fees < 0 else "+"
+        profit_rate_prefix = "-" if self.profit_rate_excluding_fees < 0 else "+"
+
+        return (
+            f"종목: {self.name} ({self.ticker})\n" +
+            f"매수금액: ${self.cost_basis} ({self.quanity}주)\n" +
+            f"평가금액(수수료제외): ${self.market_value_excluding_fees}\n" + 
+            f"손익: {profit_amount_prefix}${abs(self.profit_amount_excluding_fees)} ({profit_rate_prefix}{abs(self.profit_rate_excluding_fees * 100)}%)\n"
+        )
